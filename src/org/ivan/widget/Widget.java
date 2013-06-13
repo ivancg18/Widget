bd65d69566b2d7ef07c24973e7efbce8f49ff8e8
ivan <ivan@Portatil.Home>
2013-06-13 18:04:49 +0200

    Codigo

---
+++ src/org/ivan/widget/Widget.java
@@ -0,0 +1,15 @@
+package org.ivan.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class Widget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context,
+                 AppWidgetManager appWidgetManager,
+                 int[] appWidgetIds) {
+        //Actualizar el widget
+        //...
+    }
+}
